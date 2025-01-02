7e909986f3699e319825a06dfc6223d0d601c835
Salman Izhar <135225529+salman679@users.noreply.github.com>
2025-01-02 23:46:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const twilio = require("twilio");
 require("dotenv").config();
 
-const githubUsername = process.env.GITHUBUSERNAME;
+const githubUsername = process.env.MYUSERNAME;
 const toWhatsapp = process.env.TO_WHATSAPP;
 const fromWhatsapp = process.env.FROM_WHATSAPP;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
